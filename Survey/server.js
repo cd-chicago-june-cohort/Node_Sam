c446da3a35af20a9a9ffe2c5ec223a04ec3fc27f
sozo2 <sam.zoeller@gmail.com>
2017-08-04 14:36:31 -0500

    sock attack

--- Survey/server.js
+++ Survey/server.js
@@ -24,13 +24,28 @@ app.post('/process', function(req, res) {
         'lang' : survey_info.lang,
         'comment' : survey_info.comment
     }
-    res.redirect("results");
+    res.redirect("/");
 });
 
-app.get('/results', function(req, res) {
-    res.render("results", {"info" : req.session.info});
-});
+// app.get('/results', function(req, res) {
+//     res.render("results", {"info" : req.session.info});
+// });
 
-app.listen(8000, function() {
+var server = app.listen(8000, function() {
     console.log("listening on port 8000");
+});
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+  console.log("Client/socket is connected!");
+  console.log("Client/socket id is: ", socket.id);
+
+  socket.on("survey_submitted", function(data){
+      console.log(data.message);
+      var randnum = Math.floor(Math.random() * 1000) + 1;
+      var obj_str = JSON.stringify(data.message);
+      var message_str = 'You emitted the following information to the server: ' + obj_str; 
+      var num_str = 'Your lucky number emitted by the server is ' + randnum +'.'; 
+      socket.emit( 'server_response', {response:  message_str, lucky_num: num_str});
+  })
 });
